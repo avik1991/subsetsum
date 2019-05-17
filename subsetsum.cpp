6e27b1e59a23b6002d55ff1c22e2e9b57ed856c3
avik1991 <50135299+avik1991@users.noreply.github.com>
2019-05-17 15:05:06 +0530

    complete subset sum

--- subsetsum.cpp
+++ subsetsum.cpp
@@ -1,13 +1,59 @@
 #include <iostream> 
 using namespace std; 
   
+bool subSum(int n,int k,int arr[]){
+    cout<<"\nset :{";
+    for(int i=0;i<n;i++)
+        cout<<" "<<arr[i];
+    cout<<"}\n";
+    cout<< "k:"<<k<<" n:"<<n<<"\n";
+
+    if(k==0)
+        return true;
+    if(n==0 && k!=0)
+        return false;
+
+    if(arr[n-1]>k)
+       return subSum(n-1,k,arr);
+    else
+    {
+        return (subSum(n-1,k-arr[n-1],arr)|| subSum(n-1,k,arr));
+    }
+}
+
 int main(int argc, char** argv) 
 { 
     cout << "You have entered " << argc 
          << " arguments:" << "\n"; 
-  
-    for (int i = 0; i < argc; ++i) 
-        cout << argv[i] << "\n"; 
-  
+
+    int K=atoi(argv[1] );
+    int n=atoi( argv[2] );
+    cout <<"K:" <<K << "\n";
+    cout <<"number of elements in set :" <<argv[2] << "\n";
+    int arr[n];
+
+    cout <<"set : {";
+    int j=0;
+    for (int i = 3; i < argc; i++) {
+        //cout << argv[i] << " "; 
+        arr[j]=atoi( argv[i] );
+        cout << arr[j] << " "; 
+        j++;
+    }
+    cout <<"}\n";
+
+    bool result=subSum(n,K,arr);
+    if (result==true)
+    {
+        /* code */
+        cout<<"found \n";
+    }
+    else
+    {
+        cout<<"not found\n";
+    }
+    
+    
     return 0; 
 }
+
