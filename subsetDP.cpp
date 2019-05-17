b1b20bb3f77f8673b36da24dc86a054fd5c0a191
avik1991 <50135299+avik1991@users.noreply.github.com>
2019-05-17 15:26:43 +0530

    Dynamic Subsetsum

---
+++ subsetDP.cpp
@@ -0,0 +1,62 @@
+#include <iostream> 
+using namespace std; 
+  
+bool subSum(int n,int k,int arr[]){
+    bool DPTable[n][k];
+
+    for(int i=0;i<n;i++){
+        for (int j = 0; j < k; j++)
+        {
+            int sum=0;
+           for(int p=0;p<i;p++){
+               sum=sum+arr[p];
+           }
+           if (sum==j)
+             DPTable[i][j]= true;
+           else
+            DPTable[i][j]= false;
+
+            
+        }
+        
+    }
+
+    return DPTable[n][k];
+}
+
+int main(int argc, char** argv) 
+{ 
+    cout << "You have entered " << argc 
+         << " arguments:" << "\n"; 
+
+    int K=atoi(argv[1] );
+    int n=atoi( argv[2] );
+    cout <<"K:" <<K << "\n";
+    cout <<"number of elements in set :" <<argv[2] << "\n";
+    int arr[n];
+
+    cout <<"set : {";
+    int j=0;
+    for (int i = 3; i < argc; i++) {
+        //cout << argv[i] << " "; 
+        arr[j]=atoi( argv[i] );
+        cout << arr[j] << " "; 
+        j++;
+    }
+    cout <<"}\n";
+
+    bool result=subSum(n,K,arr);
+    if (result==true)
+    {
+        /* code */
+        cout<<"found \n";
+    }
+    else
+    {
+        cout<<"not found\n";
+    }
+    
+    
+    return 0; 
+}
+
